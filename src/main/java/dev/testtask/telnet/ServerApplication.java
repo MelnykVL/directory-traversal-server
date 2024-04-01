4780387d792c56a7cc5238418ceefae4703c0766
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-01 20:28:26 +0300

    Added 'input port from console' functionality

--- src/main/java/dev/testtask/telnet/ServerApplication.java
+++ src/main/java/dev/testtask/telnet/ServerApplication.java
@@ -2,11 +2,15 @@ package dev.testtask.telnet;
 
 import dev.testtask.telnet.server.SimpleTelnetServer;
 import dev.testtask.telnet.service.SearchService;
+import java.util.Scanner;
 
 public class ServerApplication {
 
   public static void main(String[] args) {
-    SimpleTelnetServer simpleTelnetServer = new SimpleTelnetServer(8081, new SearchService());
+    System.out.println("Type server port: ");
+    Scanner scanner = new Scanner(System.in);
+    int serverPort = scanner.nextInt();
+    SimpleTelnetServer simpleTelnetServer = new SimpleTelnetServer(serverPort, new SearchService());
     simpleTelnetServer.start();
   }
 }

90f75a167216f79c4017772f6e02bc3c0d3a6c08
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-01 20:20:27 +0300

    Fixed SearchService.java

--- src/main/java/dev/testtask/telnet/service/SearchService.java
+++ src/main/java/dev/testtask/telnet/service/SearchService.java
@@ -1,8 +1,11 @@
 package dev.testtask.telnet.service;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.Executors;
@@ -10,41 +13,42 @@ import java.util.concurrent.Future;
 
 public class SearchService {
 
-    public Future<List<String>> search(String rootPath, int depth, String mask) {
-        Queue<File> queue = new ArrayDeque<>();
-        List<String> fileList = new ArrayList<>();
-        queue.add(new File(rootPath));
-
-        while (!queue.isEmpty()) {
-            File current = queue.poll();
-            int currentDepth = this.getCurrentDepth(current, rootPath);
-
-            if (currentDepth == depth && current.getName()
-                    .contains(mask)) {
-                fileList.add(current.getAbsolutePath());
-            }
-
-            if (currentDepth < depth && current.isDirectory()) {
-                File[] files = current.listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        queue.add(file);
-                    }
-                }
-            }
-        }
-        return Executors.newCachedThreadPool()
-                .submit(() -> fileList);
+  public Future<List<String>> search(String rootPath, int depth, String mask) {
+    Queue<File> queue = new ArrayDeque<>();
+    List<String> fileList = new ArrayList<>();
+    Path path = Path.of(rootPath);
+    if (Files.exists(path)) {
+      queue.add(new File(rootPath));
     }
 
-    private int getCurrentDepth(File current, String rootPath) {
-        int depth = 0;
-        File parent = current;
-        while (!parent.getAbsolutePath()
-                .equals(rootPath)) {
-            parent = parent.getParentFile();
-            depth++;
+    while (!queue.isEmpty()) {
+      File current = queue.poll();
+      int currentDepth = this.getCurrentDepth(current, rootPath);
+
+      if (currentDepth == depth && current.getName()
+          .contains(mask)) {
+        fileList.add(current.getAbsolutePath());
+      }
+
+      if (currentDepth < depth && current.isDirectory()) {
+        File[] files = current.listFiles();
+        if (files != null) {
+          queue.addAll(Arrays.asList(files));
         }
-        return depth;
+      }
     }
+    return Executors.newCachedThreadPool()
+        .submit(() -> fileList);
+  }
+
+  private int getCurrentDepth(File current, String rootPath) {
+    int depth = -1;
+    File parent = current;
+    while (!parent.getAbsolutePath()
+        .equals(rootPath)) {
+      parent = parent.getParentFile();
+      depth++;
+    }
+    return depth;
+  }
 }

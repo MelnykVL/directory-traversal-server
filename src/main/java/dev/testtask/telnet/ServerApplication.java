501929a17c55e5d3daec9da6e5760e02f6921b12
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-03 11:40:50 +0300

    Removed ReentrantLock from ServerApplication

--- src/main/java/dev/testtask/telnet/ServerApplication.java
+++ src/main/java/dev/testtask/telnet/ServerApplication.java
@@ -1,9 +1,7 @@
 package dev.testtask.telnet;
 
 import dev.testtask.telnet.server.SimpleTelnetServer;
-import dev.testtask.telnet.service.SearchService;
 import java.util.Scanner;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class ServerApplication {
 
@@ -11,8 +9,7 @@ public class ServerApplication {
     System.out.println("Type server port: ");
     Scanner scanner = new Scanner(System.in);
     int serverPort = scanner.nextInt();
-    ReentrantLock lock = new ReentrantLock();
-    SimpleTelnetServer simpleTelnetServer = new SimpleTelnetServer(serverPort, new SearchService(lock));
+    SimpleTelnetServer simpleTelnetServer = new SimpleTelnetServer(serverPort);
     simpleTelnetServer.start();
   }
 }

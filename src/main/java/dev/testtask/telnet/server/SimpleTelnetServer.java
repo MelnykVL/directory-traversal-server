a3b0215ccc540ffca998a8a9b76cd0984c6916aa
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-03 11:40:41 +0300

    Removed SearchService from SimpleTelnetServer

--- src/main/java/dev/testtask/telnet/server/SimpleTelnetServer.java
+++ src/main/java/dev/testtask/telnet/server/SimpleTelnetServer.java
@@ -2,24 +2,25 @@ package dev.testtask.telnet.server;
 
 import dev.testtask.telnet.exception.TelnetServerSocketSetupException;
 import dev.testtask.telnet.handler.ClientRequestHandler;
-import dev.testtask.telnet.service.SearchService;
+import dev.testtask.telnet.model.ClientRequest;
+import dev.testtask.telnet.searcher.FileSearcher;
 import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @Slf4j
 public class SimpleTelnetServer {
 
   private final int port;
-  private final SearchService searchService;
   private final ExecutorService executorService;
 
-  public SimpleTelnetServer(int port, SearchService searchService) {
+  public SimpleTelnetServer(int port) {
     this.port = port;
-    this.searchService = searchService;
     this.executorService = Executors.newCachedThreadPool();
   }
 
@@ -27,10 +28,12 @@ public class SimpleTelnetServer {
     try (ServerSocket serverSocket = new ServerSocket(this.port);) {
       log.info("Server listening on port {}.", this.port);
 
+      BlockingQueue<ClientRequest> queue = new LinkedBlockingQueue<>();
+      executorService.submit(new FileSearcher(queue));
       while (true) {
         Socket clientSocket = serverSocket.accept();
         log.info("Client connected: {}", clientSocket);
-        ClientRequestHandler clientRequestHandler = new ClientRequestHandler(clientSocket, searchService);
+        ClientRequestHandler clientRequestHandler = new ClientRequestHandler(clientSocket, queue);
         executorService.submit(clientRequestHandler);
       }
     } catch (IOException ex) {

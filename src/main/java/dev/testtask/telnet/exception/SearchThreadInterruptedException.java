8e49727f8c160b6edbf81a83e747344763ea8546
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-03 11:41:27 +0300

    Removed SearchThreadInterruptedException.java

--- src/main/java/dev/testtask/telnet/exception/SearchThreadInterruptedException.java
+++
@@ -1,8 +0,0 @@
-package dev.testtask.telnet.exception;
-
-public class SearchThreadInterruptedException extends RuntimeException{
-
-  public SearchThreadInterruptedException(Throwable ex) {
-    super("Search thread was interrupted.", ex);
-  }
-}
